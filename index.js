5e43cac43a66c6388329b8fb53cefcf9010ca680
manahilnadeem <163245288+nadeemtahir@users.noreply.github.com>
2024-03-30 14:41:32 -0700

    Done

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let randomNumbers = Math.floor(Math.random() * 5 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Please guess any number between 1 - 5:",
+    },
+]);
+if (answers.userGuessNumber === randomNumbers) {
+    console.log("HURRAY! you guess correct number.");
+}
+else {
+    console.log("UPSSSS! you guess wrong number. Please try again....THANK YOU.");
+}
